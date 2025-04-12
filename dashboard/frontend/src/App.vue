82dedd1dabcf52038cd3e4c074b33440c68c4d24
alXorazmiy <alxorazmiy1997@gmail.com>
2025-04-12 12:22:47 +0500

    commit

---
+++ dashboard/frontend/src/App.vue
@@ -0,0 +1,23 @@
+
+<template>
+    <RouterView />
+</template>
+
+<script setup>
+    import { RouterLink, RouterView } from 'vue-router'
+
+</script>
+
+
+
+
+<style scoped>
+
+a.router-link-exact-active {
+    color: var(--color-text);
+}
+a.router-link-exact-active:hover {
+    background-color: transparent;
+}
+
+</style>
